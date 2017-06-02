2e0a2824c8562ca6164e9aa442fa2447781cb114
johnmartinmax <johnmartinmax999@hotmail.com>
2017-06-02 10:52:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 var kiwi_app = './server/kiwi.js';
 require(kiwi_app);
+
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
